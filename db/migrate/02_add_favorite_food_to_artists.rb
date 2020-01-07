1341826ab7de4adaeda1bb24cccc29ea5c5f53dd
Marc Poppy <poppy.marc@gmail.com>
2020-01-07 05:17:10 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_favorite_food_to_artists.rb
+++ db/migrate/02_add_favorite_food_to_artists.rb
@@ -2,5 +2,6 @@
 class AddFavoriteFoodToArtists < ActiveRecord::Migration[5.2]
   def change
     add_column :artists, :favorite_food, :string
+    #adds column to artists called fav food that will contain string 
   end
 end
